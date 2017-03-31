c1bfa1d6db4078a89e159899efdda4f5207fca28
simon.du <a932455223@qq.com>
2017-03-31 17:21:03 +0800

    demo2:async component

---
+++ demo2/src/components/App.js
@@ -0,0 +1,25 @@
+import React,{ Component } from 'react'
+
+
+const importLazy = (promise) => (
+  promise.then((result) => result.default)
+);
+
+export default class App extends Component{
+    handle = (e) =>{
+        require.ensure([],() => {
+            let com = require('./Hello');
+            this.setState({
+                com:com
+            })
+        },'Hello')
+    }
+    render(){
+        return (
+            <div>
+                <button onClick={this.handle}>click</button>
+                { this.state.com &&  <this.state.com />}
+            </div>
+        )
+    }
+}
--- demo2/src/index.jsx
+++ demo2/src/index.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import  { Layout, Menu, Breadcrumb, Icon } from 'antd';
-
-ReactDOM.render(
-  <h1>abc</h1>, document.getElementById('root'));
+import React from 'react'
+import { render } from 'react-dom'
+import App from './components/App'
+render(<App/>,document.getElementById('root'))
